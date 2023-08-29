19b0f34affb185b407a15895ce1ae333b8c225d1
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-08-29 13:50:01 -0600

    dog register almost done

--- client/src/components/pages/CreateDog.jsx
+++ client/src/components/pages/CreateDog.jsx
@@ -4,12 +4,41 @@ import useUserStore from "../../hooks/userStore";
 
 export default function CreateDog() {
   const { user } = useUserStore();
+  const [dogObj, setDogObj] = useState({
+    dog_name: "",
+    dog_breed: "",
+    dog_age: 0,
+    dog_owner_id: user.id,
+  });
+  const handleCreateDog = (e) => {
+    e.preventDefault();
+    console.log(dogObj);
+    // fetch("/api/dogs", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(dogObj),
+    // })
+    //   .then((response) => {
+    //     if (!response.ok) {
+    //       throw new Error("Network response error");
+    //     }
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     console.log(data);
+    //   })
+    //   .catch((error) => {
+    //     console.log("error", error.message);
+    //   });
+  };
   return (
     <Container fluid>
       <Row className="justify-content-center mt-4">
         <Col xs={12} md={8} lg={6}>
           <div className="border p-4 rounded">
-            <Form>
+            <Form onSubmit={handleCreateDog}>
               <Form.Group
                 className="mb-3"
                 controlId="exampleForm.ControlInput1"
@@ -18,6 +47,13 @@ export default function CreateDog() {
                 <Form.Control
                   type="text"
                   placeholder="What do you call your dog?"
+                  value={dogObj.dog_name}
+                  onChange={(e) =>
+                    setDogObj({
+                      ...dogObj,
+                      dog_name: e.target.value,
+                    })
+                  }
                 />
               </Form.Group>
               <Form.Group
@@ -28,6 +64,13 @@ export default function CreateDog() {
                 <Form.Control
                   type="text"
                   placeholder="What breed is your dog?"
+                  value={dogObj.dog_breed}
+                  onChange={(e) =>
+                    setDogObj({
+                      ...dogObj,
+                      dog_breed: e.target.value,
+                    })
+                  }
                 />
               </Form.Group>
               <Form.Group
@@ -35,7 +78,17 @@ export default function CreateDog() {
                 controlId="exampleForm.ControlTextarea1"
               >
                 <Form.Label>Dog Age</Form.Label>
-                <Form.Control type="text" placeholder="How old is your dog?" />
+                <Form.Control
+                  type="number"
+                  placeholder="How old is your dog?"
+                  value={dogObj.dog_age}
+                  onChange={(e) =>
+                    setDogObj({
+                      ...dogObj,
+                      dog_age: e.target.value,
+                    })
+                  }
+                />
               </Form.Group>
               <Button variant="primary" type="submit" className="m-1 ">
                 Register Dog!
